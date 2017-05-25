b1585dac2d4c8bb688aa53ece5d318cb285ddf13
elson Huang <elson.huang@jamzoo.com.tw>
2017-05-25 15:30:57 +0800

    coreData的新增 查詢 修改 刪除

--- coreDataTest/ViewController.swift
+++ coreDataTest/ViewController.swift
@@ -12,9 +12,55 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDel =  UIApplication.shared.delegate as? AppDelegate
+        guard let context  = appDel?.persistentContainer.viewContext else {
+           return
+        }
+//        let aUser = User(context: context)
+//        aUser.username = "elson"
+//        aUser.password = "123456"
+//        appDel?.saveContext()
+        
+        
+//        do {
+//            let results = try context.fetch(User.fetchRequest())
+//            let testUser = results[0] as? User
+//            testUser?.password = "testPassword"
+//            appDel?.saveContext()
+//        } catch  {
+//            
+//        }
+        
+        do {
+           let results = try context.fetch(User.fetchRequest())
+            for item in results{
+                guard let nowUser = item as? User else{return
+                }
+              
+                if nowUser.username == "elson"{
+                    context.delete(nowUser)
+                    appDel?.saveContext()
+                }
+                
+            }
+        } catch  {
+            
+        }
+        
+        
+                do {
+                   let results = try context.fetch(User.fetchRequest())
+                    for item in results{
+                  let nowUser =   item as? User
+                   print("\(nowUser?.username)")
+                print("\(nowUser?.password)")
+                    }
+                } catch  {
+                    
+                }
+    
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
